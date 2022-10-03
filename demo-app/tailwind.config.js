a902193fc62f2a8e2ca2d604edf7b48e086e0980
OMikkel <mikkelmaae@gmail.com>
2022-10-03 23:25:21 +0200

    Setup environment for development

---
+++ demo-app/tailwind.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+module.exports = {
+    content: [
+        "./components/**/*.tsx",
+        "./pages/**/*.{ts,tsx}",
+        "./public/**/*.html",
+    ],
+    plugins: [],
+    theme: {},
+};
--- extras/twoDigitToFullYear/test/twoDigitToFullYear.js
+++ extras/twoDigitToFullYear/test/twoDigitToFullYear.js
@@ -1,58 +1,59 @@
-import {twoDigitToFullYear} from '../twoDigitToFullYear.js';
+import { twoDigitToFullYear } from '../twoDigitToFullYear.js';
 
 const expect = require('unexpected');
 const sinon = require('sinon');
 
 describe('twoDigitToFullYear()', function () {
-  it('converts 2-digit year to full year using 50 for the future years range by default', function () {
-    let clock = sinon.useFakeTimers({now: new Date().setFullYear(2020)});
 
-    expect(twoDigitToFullYear(0), 'to be', 2000);
-    expect(twoDigitToFullYear(20), 'to be', 2020);
-    expect(twoDigitToFullYear(70), 'to be', 2070);
-    expect(twoDigitToFullYear(71), 'to be', 1971);
+    it('converts 2-digit year to full year using 50 for the future years range by default', function () {
+        let clock = sinon.useFakeTimers({ now: new Date().setFullYear(2020) });
 
-    clock.restore();
-    clock = sinon.useFakeTimers({now: new Date().setFullYear(2080)});
+        expect(twoDigitToFullYear(0), 'to be', 2000);
+        expect(twoDigitToFullYear(20), 'to be', 2020);
+        expect(twoDigitToFullYear(70), 'to be', 2070);
+        expect(twoDigitToFullYear(71), 'to be', 1971);
 
-    expect(twoDigitToFullYear(0), 'to be', 2100);
-    expect(twoDigitToFullYear(30), 'to be', 2130);
-    expect(twoDigitToFullYear(31), 'to be', 2031);
-    expect(twoDigitToFullYear(80), 'to be', 2080);
+        clock.restore();
+        clock = sinon.useFakeTimers({ now: new Date().setFullYear(2080) });
 
-    clock.restore();
-  });
+        expect(twoDigitToFullYear(0), 'to be', 2100);
+        expect(twoDigitToFullYear(30), 'to be', 2130);
+        expect(twoDigitToFullYear(31), 'to be', 2031);
+        expect(twoDigitToFullYear(80), 'to be', 2080);
 
-  it('uses sepecified future year range if it is given', function () {
-    let clock = sinon.useFakeTimers({now: new Date().setFullYear(2020)});
+        clock.restore();
+    });
 
-    expect(twoDigitToFullYear(40, 20), 'to be', 2040);
-    expect(twoDigitToFullYear(40, 19), 'to be', 1940);
-    expect(twoDigitToFullYear(95, 75), 'to be', 2095);
-    expect(twoDigitToFullYear(95, 74), 'to be', 1995);
+    it('uses sepecified future year range if it is given', function () {
+        let clock = sinon.useFakeTimers({ now: new Date().setFullYear(2020) });
 
-    clock.restore();
-    clock = sinon.useFakeTimers({now: new Date().setFullYear(2080)});
+        expect(twoDigitToFullYear(40, 20), 'to be', 2040);
+        expect(twoDigitToFullYear(40, 19), 'to be', 1940);
+        expect(twoDigitToFullYear(95, 75), 'to be', 2095);
+        expect(twoDigitToFullYear(95, 74), 'to be', 1995);
 
-    expect(twoDigitToFullYear(0, 20), 'to be', 2100);
-    expect(twoDigitToFullYear(0, 19), 'to be', 2000);
-    expect(twoDigitToFullYear(55, 75), 'to be', 2155);
-    expect(twoDigitToFullYear(55, 74), 'to be', 2055);
+        clock.restore();
+        clock = sinon.useFakeTimers({ now: new Date().setFullYear(2080) });
 
-    clock.restore();
-  });
+        expect(twoDigitToFullYear(0, 20), 'to be', 2100);
+        expect(twoDigitToFullYear(0, 19), 'to be', 2000);
+        expect(twoDigitToFullYear(55, 75), 'to be', 2155);
+        expect(twoDigitToFullYear(55, 74), 'to be', 2055);
 
-  it('uses the century of baseDate for conversion if it is given', function () {
-    const year2020 = new Date(2020, 0, 1);
-    expect(twoDigitToFullYear(40, 20, year2020), 'to be', 2040);
-    expect(twoDigitToFullYear(40, 19, year2020), 'to be', 1940);
-    expect(twoDigitToFullYear(95, 75, year2020), 'to be', 2095);
-    expect(twoDigitToFullYear(95, 74, year2020), 'to be', 1995);
+        clock.restore();
+    });
 
-    const year2280 = new Date(2280, 0, 1);
-    expect(twoDigitToFullYear(0, 20, year2280), 'to be', 2300);
-    expect(twoDigitToFullYear(0, 19, year2280), 'to be', 2200);
-    expect(twoDigitToFullYear(55, 75, year2280), 'to be', 2355);
-    expect(twoDigitToFullYear(55, 74, year2280), 'to be', 2255);
-  });
+    it('uses the century of baseDate for conversion if it is given', function () {
+        const year2020 = new Date(2020, 0, 1);
+        expect(twoDigitToFullYear(40, 20, year2020), 'to be', 2040);
+        expect(twoDigitToFullYear(40, 19, year2020), 'to be', 1940);
+        expect(twoDigitToFullYear(95, 75, year2020), 'to be', 2095);
+        expect(twoDigitToFullYear(95, 74, year2020), 'to be', 1995);
+
+        const year2280 = new Date(2280, 0, 1);
+        expect(twoDigitToFullYear(0, 20, year2280), 'to be', 2300);
+        expect(twoDigitToFullYear(0, 19, year2280), 'to be', 2200);
+        expect(twoDigitToFullYear(55, 75, year2280), 'to be', 2355);
+        expect(twoDigitToFullYear(55, 74, year2280), 'to be', 2255);
+    });
 });
