7973dfd756d2ec2f277f173effa4947981e182e6
OMikkel <mikkelmaae@gmail.com>
2022-10-04 17:18:00 +0200

    Deploy nextjs demo-app

--- demo-app/next.config.js
+++ demo-app/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
 
 module.exports = nextConfig
