cee074858e19e02c7a2e2ada067d3005aacb5f1a
OMikkel <57924435+OMikkel@users.noreply.github.com>
2022-10-04 20:54:24 +0000

    Deploying to gh-pages from @ OMikkel/tailwind-datepicker-react@a1d9cf73d604f980720b097f95d66a2ade3237f5 🚀

---
+++ _next/static/WUohldMG_AAUXLdfB8epc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c3e6f1f36ce90155.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/nSy0ZJxesCZBpr99xAZpI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9e9731b4c3d1ac78.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
