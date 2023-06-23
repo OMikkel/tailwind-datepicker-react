a3603113bca3f1163851394e639c2cd505af684c
OMikkel <57924435+OMikkel@users.noreply.github.com>
2023-06-23 10:23:59 +0000

    Deploying to gh-pages from @ OMikkel/tailwind-datepicker-react@5974cf6748c12d2a5bd97ed3caa5a578382645af 🚀

---
+++ _next/static/8L5eAZTt01LLD5Bd6gNvr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/745-910b99cf63b3cd36.js","static/chunks/pages/index-9b576d888f6e7bf6.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/dqLOXNIlDUoBh2L9ITMVM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/745-910b99cf63b3cd36.js","static/chunks/pages/index-e1ea1f3d5971b7d8.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
